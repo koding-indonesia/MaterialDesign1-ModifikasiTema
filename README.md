4ecd4a4b0aa04db80bb97614725f23a0093edf1d
Andry Lubis <andryldev@yahoo.com>
2017-06-06 04:13:17 +0700

    Publish Source

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MaterialDesign1-ModifikasiTema
-User Interface (UI) Material Design - Modifikasi Tema
+User Interface (UI) - Modifikasi Tema Dengan Material Design
+
+Terdiri dari 3 file yang telah dimodifikasi, yaitu:
+1. color.xml
+2. styles.xml, dan
+3. AndroidManifest.xml.
+
---
+++ app/src/main/java/myusuf/kodingindonesia/com/edittheme/MainActivity.java
@@ -0,0 +1,37 @@
+package myusuf.kodingindonesia.com.edittheme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public boolean onOptionItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
